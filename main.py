8d6f580985e7a3c715971a11f6b180117181951f
liuabin <hzlxb123@gmail.com>
2021-10-15 17:29:47 +0800

    init project

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # 游戏 main
 
+from click import getchar
+
+# 常量
+QUIT = 'q'
+
 def main():
     print("hello game.")
+    
+    # 游戏主循环
+    c = ''
+    while c != QUIT:
+        c = getchar()
+        print(c)
 
 main()
